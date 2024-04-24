7c7e3b382313f34d0e7d59e6232d949fce40b4e2
Tyr Chen <tyr.chen@gmail.com>
2024-04-23 20:55:49 -0700

    feature: sql design

---
+++ sql/init.sql
@@ -0,0 +1,49 @@
+-- this file is used for postgresql database initialization
+-- create user table
+CREATE TABLE IF NOT EXISTS users(
+  id bigserial PRIMARY KEY,
+  fullname varchar(64) NOT NULL,
+  email varchar(64) NOT NULL,
+  -- hashed argon2 password
+  password VARCHAR(64) NOT NULL,
+  created_at timestamp DEFAULT CURRENT_TIMESTAMP
+);
+
+-- create index for users for email
+CREATE UNIQUE INDEX IF NOT EXISTS email_index ON users(email);
+
+-- create chat type: single, group, private_channel, public_channel
+CREATE TYPE chat_type AS ENUM(
+  'single',
+  'group',
+  'private_channel',
+  'public_channel'
+);
+
+-- create chat table
+CREATE TABLE IF NOT EXISTS chats(
+  id bigserial PRIMARY KEY,
+  name varchar(128) NOT NULL UNIQUE,
+  type chat_type NOT NULL,
+  -- user id list
+  members bigint[] NOT NULL,
+  created_at timestamp DEFAULT CURRENT_TIMESTAMP
+);
+
+-- create message table
+CREATE TABLE IF NOT EXISTS messages(
+  id bigserial PRIMARY KEY,
+  chat_id bigint NOT NULL,
+  sender_id bigint NOT NULL,
+  content text NOT NULL,
+  images text[],
+  created_at timestamp DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (chat_id) REFERENCES chats(id),
+  FOREIGN KEY (sender_id) REFERENCES users(id),
+);
+
+-- create index for messages for chat_id and created_at order by created_at desc
+CREATE INDEX IF NOT EXISTS chat_id_created_at_index ON messages(chat_id, created_at DESC);
+
+-- create index for messages for sender_id
+CREATE INDEX IF NOT EXISTS sender_id_index ON messages(sender_id);
