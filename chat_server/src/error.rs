b10b29607b89d1c5529daf7327f547cd86b537a8
Tyr Chen <tyr.chen@gmail.com>
2024-06-09 22:10:46 -0700

    fix: jwt::Error is anyhow::Error, make it general (btw, no need to return forbidden for jwt error, as auth middleware already did so, and all rest cases should be internal server error)

--- chat_server/src/error.rs
+++ chat_server/src/error.rs
@@ -36,8 +36,8 @@ pub enum AppError {
     #[error("password hash error: {0}")]
     PasswordHashError(#[from] argon2::password_hash::Error),
 
-    #[error("jwt error: {0}")]
-    JwtError(#[from] jwt_simple::Error),
+    #[error("general error: {0}")]
+    AnyError(#[from] anyhow::Error),
 
     #[error("http header parse error: {0}")]
     HttpHeaderError(#[from] axum::http::header::InvalidHeaderValue),
@@ -56,7 +56,7 @@ impl IntoResponse for AppError {
         let status = match &self {
             Self::SqlxError(_) => StatusCode::INTERNAL_SERVER_ERROR,
             Self::PasswordHashError(_) => StatusCode::UNPROCESSABLE_ENTITY,
-            Self::JwtError(_) => StatusCode::FORBIDDEN,
+            Self::AnyError(_) => StatusCode::INTERNAL_SERVER_ERROR,
             Self::HttpHeaderError(_) => StatusCode::UNPROCESSABLE_ENTITY,
             Self::EmailAlreadyExists(_) => StatusCode::CONFLICT,
             Self::CreateChatError(_) => StatusCode::BAD_REQUEST,
